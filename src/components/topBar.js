44e0745d36d0c3b9f619d0b13de83cb9da49e49a
James Xie <James Xie>
2017-04-22 15:37:15 -0500

    First push

---
+++ src/components/topBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/topBar.css';
+
+class topBar extends Component {
+  render() {
+    return (
+      <div>YES</div>
+    );
+  }
+}
+
+export default topBar;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import topBar from './components/topBar.js';
+import './index.css';
+
+ReactDOM.render(
+  <topBar />,
+  document.getElementById('topBar')
+);
